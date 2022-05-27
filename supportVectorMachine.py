1427abd5aa9439b68d5139d69882b4dc30b2e882
DeronJMartin <deronudemartin@gmail.com>
2022-05-27 04:56:45 -0700

    Fixed SK_SVM

--- supportVectorMachine.py
+++ supportVectorMachine.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 class supportVectorClassifier():
     def __init__(self):
@@ -9,5 +10,15 @@ class supportVectorClassifier():
     def fit(self, x_train, y_train):
         pass
 
-    def predict(self, x_test, y_test, metrics):
+    def predict(self, x_test, y_test):
         pass
+
+if __name__ == "__main__":
+    # Import data
+    spotifyData = pd.read_csv('data1.csv')
+    
+    # Drop irrelevant classes and seperate features and classes
+    x = spotifyData.drop(['artist', 'target', 'song_title', 'serial_num'], axis=1)
+    y = spotifyData['target']
+
+    print(x.shape)
